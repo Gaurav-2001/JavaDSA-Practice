cdcc70bcc9eccb646febbdffae32fec97660ab94
Gaurav Pagare <gauravpagare2001@gmail.com>
2022-08-13 03:52:18 +0530

    Updated

---
+++ src/com/gaurav/PracticeProblemsFromVideo/Arrays/Two.java
@@ -0,0 +1,14 @@
+package com.gaurav.PracticeProblemsFromVideo.Arrays;
+
+public class Two {
+    public static void main(String[] args) {
+        int[] arr = {4,18,7,56,5,40};
+        int max=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
