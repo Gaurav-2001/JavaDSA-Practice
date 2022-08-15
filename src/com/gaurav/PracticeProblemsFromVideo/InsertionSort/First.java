037924c3ea9c9247b44708249da5f7215820f337
Gaurav Pagare <gauravpagare2001@gmail.com>
2022-08-15 14:07:21 +0530

    Updated

---
+++ src/com/gaurav/PracticeProblemsFromVideo/InsertionSort/First.java
@@ -0,0 +1,27 @@
+package com.gaurav.PracticeProblemsFromVideo.InsertionSort;
+
+import java.util.Arrays;
+
+public class First {
+    public static void main(String[] args) {
+        int[] arr = {5,3,4,1,2};
+        SelectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void SelectionSort(int[] arr){
+        //for outer loop
+        for(int i=0;i<=arr.length-2;i++){
+            //for inner loop
+            for(int j=i+1;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+}
