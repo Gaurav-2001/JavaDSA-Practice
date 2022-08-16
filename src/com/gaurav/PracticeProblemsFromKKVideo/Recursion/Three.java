b0da57ed668dfde1c4c20a2fcec200db0f5736ee
Gaurav Pagare <gauravpagare2001@gmail.com>
2022-08-16 12:42:53 +0530

    Updated

--- src/com/gaurav/PracticeProblemsFromKKVideo/Recursion/Three.java
+++ src/com/gaurav/PracticeProblemsFromKKVideo/Recursion/Three.java
@@ -2,6 +2,20 @@ package com.gaurav.PracticeProblemsFromKKVideo.Recursion;
 
 public class Three {
     public static void main(String[] args) {
-
+        int[] arr = {3,4,6,8,13,15,20,23,66,78,90};
+        System.out.println(BinarySearch(arr,66,0, arr.length-1));
+    }
+    static int BinarySearch(int[] arr, int target, int start, int end){
+        if(start>end){
+            return -1;
+        }
+        int mid = start + (end -start)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(target<arr[mid]){
+            return BinarySearch(arr,target,start,mid-1);
+        }
+        return BinarySearch(arr,target,mid+1,end);
     }
 }
